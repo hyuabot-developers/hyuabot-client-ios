62b093c51c25e335300c51cc541e438f1213fc4b
Jeongin Lee <jil8885@gmail.com>
2024-12-02 17:49:53 +0900

    Initial Commit

---
+++ hyuabot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hyuabot
+//
+//  Created by 이정인 on 12/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hyuabot/hyuabotApp.swift
@@ -0,0 +1,17 @@
+//
+//  hyuabotApp.swift
+//  hyuabot
+//
+//  Created by 이정인 on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct hyuabotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
