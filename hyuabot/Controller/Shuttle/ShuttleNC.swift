d9596fdfe6152d503f8a3eee3ea7e8de78847b4a
Jeongin Lee <jil8885@gmail.com>
2025-01-07 12:49:30 +0900

    feat: Hide navigation bar on shuttle pages

--- hyuabot/Controller/Shuttle/ShuttleNC.swift
+++ hyuabot/Controller/Shuttle/ShuttleNC.swift
@@ -4,4 +4,12 @@ class ShuttleNC: UINavigationController {
     convenience init() {
         self.init(rootViewController: ShuttleRealtimeVC())
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.setNavigationBarHidden(false, animated: false)
+    }
 }
