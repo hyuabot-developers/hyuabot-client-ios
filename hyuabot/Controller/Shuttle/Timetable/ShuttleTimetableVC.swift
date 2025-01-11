294f5c93da381b1e0a9a43a56f0b695652a84e74
Jeongin Lee <jil8885@gmail.com>
2025-01-11 21:52:12 +0900

    feat: Implement shuttle timetable filter button

--- hyuabot/Controller/Shuttle/Timetable/ShuttleTimetableVC.swift
+++ hyuabot/Controller/Shuttle/Timetable/ShuttleTimetableVC.swift
@@ -17,6 +17,15 @@ class ShuttleTimetableVC: UIViewController {
         viewPager.contentView.pages = [weekdaysVC.view, weekendsVC.view]
         return viewPager
     }()
+    private lazy var filterButton = UIButton().then {
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .hanyangGreen
+        config.cornerStyle = .medium
+        config.image = UIImage(systemName: "line.3.horizontal.decrease")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 20, weight: .regular))
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        $0.configuration = config
+        $0.addTarget(self, action: #selector(openFilterVC), for: .touchUpInside)
+    }
     
     init(stopID: String.LocalizationValue, destination: String.LocalizationValue) {
         self.stopID = stopID
@@ -51,10 +60,16 @@ class ShuttleTimetableVC: UIViewController {
     private func setupUI() {
         self.view.backgroundColor = .hanyangBlue
         self.view.addSubview(viewPager)
+        self.view.addSubview(filterButton)
         self.viewPager.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
             make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
+        self.filterButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).inset(20)
+            make.width.height.equalTo(50)
         }
     }
     
@@ -63,5 +78,14 @@ class ShuttleTimetableVC: UIViewController {
             guard let options = options else { return }
             self.navigationItem.title = "\(String(localized: options.start)) → \(String(localized: options.end))"
         }).disposed(by: disposeBag)
+    }
+    
+    @objc private func openFilterVC() {
+        let vc = ShuttleTimetableFilterVC()
+        if let sheet = vc.sheetPresentationController {
+            sheet.detents = [.large()]
+            sheet.prefersGrabberVisible = true
+        }
+        self.present(vc, animated: true, completion: nil)
     }
 }
