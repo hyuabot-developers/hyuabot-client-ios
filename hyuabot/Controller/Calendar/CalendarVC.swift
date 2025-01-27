5fa8ca49317b2bdb5f1583cb0969fbfedcfff795
Jeongin Lee <jil8885@gmail.com>
2025-01-27 19:58:17 +0900

    feat: Implement calendar UI

--- hyuabot/Controller/Calendar/CalendarVC.swift
+++ hyuabot/Controller/Calendar/CalendarVC.swift
@@ -1,7 +1,28 @@
 import UIKit
 
 class CalendarVC: UIViewController {
+    private lazy var calenadrView = {
+        let calendarView = UICalendarView()
+        calendarView.delegate = self
+        calendarView.selectionBehavior = .none
+        calendarView.tintColor = .plainButtonText
+        return calendarView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupUI()
     }
+    
+    private func setupUI() {
+        self.view.addSubview(self.calenadrView)
+        self.calenadrView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(500)
+        }
+    }
+}
+
+extension CalendarVC: UICalendarViewDelegate {
+    
 }
