102bb5b55e160583638abfe4be7b579953b20659
Jeongin Lee <jil8885@gmail.com>
2025-04-12 22:08:38 +0900

    feat: Implement watchOS target

---
+++ watch/watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchApp.swift
+//  watch Watch App
+//
+//  Created by 이정인 on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
