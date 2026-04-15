9a95040969933be7c838cc896ad0f288673704db
Jeongin Lee <jil8885@gmail.com>
2026-04-15 13:10:16 +0900

    refactor: Update latest graphQL query for watch app

--- watch/DepartureListViewModel.swift
+++ watch/DepartureListViewModel.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import RxSwift
-import QueryAPI
+import Api
 
 class DepartureListViewModel: ObservableObject {
     init(stop: String) {
@@ -11,53 +11,20 @@ class DepartureListViewModel: ObservableObject {
                 self.isLoading = loading
             })
             .disposed(by: disposeBag)
-        if (self.stop == "기숙사") {
-            ShuttleRealtimeData.shared.shuttleDormitoryData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        } else if (self.stop == "셔틀콕") {
-            ShuttleRealtimeData.shared.shuttleShuttlecockData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        } else if (self.stop == "한대앞") {
-            ShuttleRealtimeData.shared.shuttleStationData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        } else if (self.stop == "예술인") {
-            ShuttleRealtimeData.shared.shuttleTerminalData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        } else if (self.stop == "중앙역") {
-            ShuttleRealtimeData.shared.shuttleJungangStatioData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        } else if (self.stop == "셔틀콕 건너편") {
-            ShuttleRealtimeData.shared.shuttleShuttlecockOppositeData
-                .subscribe(onNext: { [weak self] shuttleData in
-                    guard let self = self else { return }
-                    self.items = shuttleData ?? []
-                })
-                .disposed(by: disposeBag)
-        }
+        Observable.combineLatest(
+            ShuttleRealtimeData.shared.firstItem,
+            ShuttleRealtimeData.shared.secondItem,
+            ShuttleRealtimeData.shared.thirdItem,
+            ShuttleRealtimeData.shared.fourthItem
+        ).subscribe(onNext: { [weak self] first, second, third, fourth in
+            guard let self = self else { return }
+            let items = [first, second, third, fourth].compactMap { $0 }
+            self.items = items
+        }).disposed(by: disposeBag)
     }
     
     private let stop: String
     private let disposeBag = DisposeBag()
-    @Published var items: [ShuttleRealtimePageQuery.Data.Shuttle.Timetable] = []
+    @Published var items: [ShuttleRealtimePageWatchQuery.Data.Shuttle.Stop.Timetable.Destination.Entry] = []
     @Published var isLoading: Bool = false
 }
