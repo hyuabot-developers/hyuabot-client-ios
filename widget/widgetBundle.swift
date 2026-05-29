17a7af21ceebb5762706ab7e7bcdde69e9d1a607
Jeongin Lee <jil8885@gmail.com>
2026-05-29 13:09:09 +0900

    feat: Implement widget extension

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by 이정인 on 5/29/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
