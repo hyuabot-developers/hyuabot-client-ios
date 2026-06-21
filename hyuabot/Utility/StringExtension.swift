3b6e104f9f5c4360fb0a077aeab740dfc75d580b
Jeongin Lee <jil8885@gmail.com>
2026-06-21 16:13:39 +0900

    fix: Guard string substring bounds

--- hyuabot/Utility/StringExtension.swift
+++ hyuabot/Utility/StringExtension.swift
@@ -1,6 +1,6 @@
 extension String {
     func substring(from: Int, to: Int) -> String {
-        guard from < count, to >= 0, to - from >= 0 else { return "" }
+        guard from >= 0, from < count, to >= 0, to < count, to >= from else { return "" }
         let startIndex = self.index(self.startIndex, offsetBy: from)
         let endIndex = self.index(self.startIndex, offsetBy: to)
         return String(self[startIndex...endIndex])
